5eeeea20d627638106f3cf6f9224b4cbf16df576
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-02-02 16:53:03 -0600

    Added require_relative/require after necessary file creations.

--- lib/sports_today.rb
+++ lib/sports_today.rb
@@ -1,4 +1,14 @@
-require "sports_today/version"
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+
+
+require_relative "sports_today/version"
+require_relative "sports_today/cli"
+require_relative "sports_today/schedule"
+require_relative "sports_today/scraper"
+
 
 module SportsToday
   # Your code goes here...
