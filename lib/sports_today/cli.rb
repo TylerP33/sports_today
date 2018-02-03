e9424075947431fc1acde5e2f8e9ed250f205bcb
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-02-03 08:50:49 -0600

    Added start and list method to CLI.  Started working on UI

--- lib/sports_today/cli.rb
+++ lib/sports_today/cli.rb
@@ -1,7 +1,41 @@
 class SportsToday::CLI
 
 def call
-	puts "Hello World"
+	start
+end
+
+def start
+	puts "---------------------------------"
+	puts "Welcome to the World of Sports!"
+	puts "---------------------------------"
+	puts ""
+	list
+	puts ""
+	puts "Here is a list of sports.  To see if your preferred sport is on today, type in the specific sport below:"
+	puts ""
+	puts ""
+	puts ""
+	
+	input = nil
+	input = gets.strip
+
+	if input == "Basketball"
+		puts "hello"
+	end
+end
+
+
+
+
+def list
+	puts "\n1. Basketball"
+	puts "\n2. Football"
+	puts "\n3. Baseball"
+	puts "\n4. Racing"
+	puts "\n5. Tennis"
+	puts "\n6. Horse Racing"
+	puts "\n7. Auto Racing"
+	puts "\n8. Track and Field"
 end
 
 end
