b868e495f07ae3c1c51917eda5312a6530146762
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-02-05 13:01:25 -0600

    Added new_from_schedule, all, find class methods
    Built a custom constructor in self.new_from_schedule.  Built self.all method to collect instances.  Built a find method so that I could display it properly in CLI.

--- lib/sports_today/schedule.rb
+++ lib/sports_today/schedule.rb
@@ -1,3 +1,31 @@
 class SportsToday::Schedule
 
+	attr_accessor :sport, :description, :time
+
+	@@all = []
+
+	def self.new_from_schedule(schedule)
+	  self.new(
+		schedule.search("h3").text, #sport
+		schedule.search("p.listings-program-description").text, #description
+		schedule.search("p.listings-program-airing-info").text #time
+		)
+	end
+
+
+	def initialize(sport=nil, description=nil, time=nil)
+		@sport = sport
+		@description = description
+		@time = time
+		@@all << self
+	end
+
+	def self.all 
+		@@all
+	end
+
+	def self.find(id)
+		self.all[id-1]
+	end
 end 
+
